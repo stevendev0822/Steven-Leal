49c055b7488eed73579feefecb4d53ac460989b2
LovelyBTC <alenschmitt415@gmail.com>
2024-11-14 10:31:08 -0600

    [update] updated the blogs published order

--- app/components/homepage/blog/blog-card.jsx
+++ app/components/homepage/blog/blog-card.jsx
@@ -20,7 +20,7 @@ function BlogCard({ blog }) {
         />
       </div>
       <div className="p-2 sm:p-3 flex flex-col">
-        <div className="flex justify-between items-center text-[#16f2b3] text-sm">
+        <div className="flex justify-between items-center text-[#16f2b3] text-sm gap-2">
           <p>{timeConverter(blog.published_at)}</p>
           <div className="flex items-center gap-3">
             <p className="flex items-center gap-1">
@@ -43,10 +43,10 @@ function BlogCard({ blog }) {
         <p className='mb-2 text-sm text-[#16f2b3]'>
           {`${blog.reading_time_minutes} Min Read`}
         </p>
-        <p className='text-sm lg:text-base text-[#d3d8e8] pb-3 lg:pb-6 line-clamp-3'>
+        <p className='text-sm lg:text-base text-[#d3d8e8] pb-3 lg:pb-6 line-clamp-3 mb-8'>
           {blog.description}
         </p>
-        <div className="">
+        <div className="absolute bottom-4 left-2">
           <Link target='_blank' href={blog.url}>
             <button className='bg-violet-500 text-white px-3 py-1.5 rounded-full text-xs'>
               Read More
